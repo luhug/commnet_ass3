aea487f7aa544a9c45e78def4d8c0cb423fd81ae
Luzian Hug <Luzian@luzian-hug.home>
2017-05-29 18:46:23 +0200

    Commit by script

--- receiver.py
+++ receiver.py
@@ -220,7 +220,7 @@ class GBNReceiver(Automaton):
                             break                            
                         #Generate contigious blocks
                         
-                        while x % 2**n_bits ~= self.next-1:
+                        while x % 2**n_bits != self.next-1:
                             if x in self.buffer:
                                 if first:
                                     sackstart.append(x)
