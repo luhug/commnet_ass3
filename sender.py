4263d53dd9d2f0d51ab6ee4c49917b771d2c21ef
Luzian Hug <Luzian@luzian-hug.home>
2017-05-29 20:12:42 +0200

    Commit by script

--- sender.py
+++ sender.py
@@ -249,7 +249,7 @@ class GBNSender(Automaton):
                                   + range(pkt.getlayer(GBN).sackstart2,pkt.getlayer(GBN).sackstart2+pkt.getlayer(GBN).sacklen2)
                                   + range(pkt.getlayer(GBN).sackstart3,pkt.getlayer(GBN).sackstart3+pkt.getlayer(GBN).sacklen3))
                     for x in range(0,last):
-                        if (~(x in sacklist)) and (x in self.buffer):
+                        if (x not in sacklist) and (x in self.buffer):
                             log.debug("SACK trigerred for packet %s. Sack List: %s" , x, str(sacklist))
                             header_GBN = GBN(type='data',
                                          options=1,
