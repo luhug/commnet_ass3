031accd83263620a05c19bbb4c2530bb0385e257
Luzian Hug <Luzian@luzian-hug.home>
2017-05-31 16:05:15 +0200

    Commit by script

--- sender.py
+++ sender.py
@@ -120,7 +120,7 @@ class GBNSender(Automaton):
         log.debug("Current sending buffer: %s" % str(self.buffer.keys()))
         # check if you still can send new packets to the receiver
         log.debug("Current: %s Unack: %s" , str(self.current),str(self.unack))
-        if len(self.buffer) < min(self.win, self.receiver_win) and (self.current - self.unack)%2**n_bits < self.win):
+        if len(self.buffer) < min(self.win, self.receiver_win) and (self.current - self.unack)%2**n_bits < self.win:
             try:
                 # get next payload (automatically removes it from queue)
                 payload = self.q.get(block=False)
