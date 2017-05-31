aaa84980da2a3668fae4213fc1a1e03fffdfcdba
Luzian Hug <Luzian@luzian-hug.home>
2017-05-31 18:45:07 +0200

    Final Commit

--- start_local.sh
+++ start_local.sh
@@ -28,11 +28,11 @@ RECEIVER_FILE="receiver.py"
 SENDER_FILE="sender.py"
 
 # Parameters for sender and receiver
-NBITS=8                    # The number of bits used to encode the sequence number
+NBITS=5                    # The number of bits used to encode the sequence number
 
 # Parameters for sender
 IN_FILE=to_send_test.txt    # Data to transmit (e.g. sample_text.txt or ETH_logo.png)
-SENDER_WIN_SIZE=128        # Window size of the sender
+SENDER_WIN_SIZE=4        # Window size of the sender
 Q_3_2=0                    # Use Selective Repeat implemented in question 3.2 (0 or 1)
 Q_3_3=1                    # Use Selective Acknowledgments implemented in question 3.3 (0 or 1)
 Q_3_4=0                    # Use Congestion Control implemented in question 3.4/Bonus (0 or 1)
